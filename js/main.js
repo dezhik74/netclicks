6d629c21df896c137eeb693ce0263e49dee07f6c
dezhik74-linux <dezhik@yandex.ru>
2020-05-26 23:24:11 +0300

    День 2  неполный - остановился на загрузке карточ.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,59 @@
-// menu
+const IMG_URL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2";
 
 const leftMenu = document.querySelector('.left-menu')
 const hamburger = document.querySelector('.hamburger')
+const tvShowsList = document.querySelector('.tv-shows__list')
+const modal = document.querySelector('.modal')
+
+const DBService = class {
+  getData = async url => {
+    const res = await fetch (url);
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error (`Не удалось получить данные по адресу ${url}`)
+    }
+
+  }
+  getTestData = () => {
+    return this.getData ('test.json')
+  }
+}
+
+const renderCard = responce => {
+
+  tvShowsList.textContent='';
+
+  responce.results.forEach(item => {
+
+    const {
+      backdrop_path: backdrop, 
+      name: title, 
+      poster_path: poster, 
+      vote_average: vote
+    } = item;
+
+    console.log(item);
+    const card = document.createElement ('li');
+    card.classList.add('tv-shows__item');
+    card.innerHTML = `
+      <a href="#" class="tv-card">
+        <span class="tv-card__vote">${vote}</span>
+        <img class="tv-card__img"
+            src="${IMG_URL + poster}"
+            data-backdrop="${IMG_URL + backdrop}"
+            alt="${title}">
+        <h4 class="tv-card__head">${title}</h4>
+      </a>
+    `;
+    tvShowsList.append(card);
+  });
+}
+
+new DBService().getTestData().then(renderCard);
+
+
+//меню
 
 hamburger.addEventListener('click', () => {
   leftMenu.classList.toggle('openMenu')
@@ -25,18 +77,41 @@ leftMenu.addEventListener('click', event => {
   }
 })
 
-function swapAttrs (elem) {
-  let tmp = elem.src
-  elem.setAttribute('src', elem.dataset.backdrop)
-  elem.setAttribute('data-backdrop', tmp)
+//смена карточки
+
+const swapAttrs = elem => {
+  const card = event.target.closest('.tv-shows__item');
+  if (card) {
+    const img = card.querySelector('.tv-card__img');
+    if (img.dataset.backdrop) {
+      [img.src, img.dataset.backdrop] = [img.dataset.backdrop, img.src]
+    }
+  }
 }
 
-Array.from(document.querySelectorAll ('.tv-card__img')).map( item => {
-  item.addEventListener('mouseover', event => {
-    swapAttrs(event.target)
-  })
-  item.addEventListener('mouseout', event => {
-    swapAttrs(event.target)
-  })
+tvShowsList.addEventListener('mouseover', swapAttrs);
+tvShowsList.addEventListener('mouseout', swapAttrs);
+
+//включение модального окна
+
+tvShowsList.addEventListener('click', event => {
+  event.preventDefault()
+  const target = event.target
+  const card = target.closest('.tv-card')
+  if (card) {
+    // modal.style.backgroundColor = 'rgba(0,0,0,4)'
+    document.body.style.overfow='hidden'
+      modal.classList.remove('hide')
+  }
+})
+
+// выключение модального окна
+
+modal.addEventListener('click', event => {
+  if (event.target.closest('.cross') || 
+      event.target.classList.contains('modal')) {
+    document.body.style.overflow=''
+    modal.classList.add('hide')
+  }
 })
 
